c6bd529f8c400f748da0923b08fe7d85aa2dcf0d
dingbisheng <504503041@qq.com>
2018-04-11 11:32:29 +0800

    1.3  dingbisheng    增加系统配置类

---
+++ src/main/java/com/damai/concert/sysconfig/SystemCfg.java
@@ -0,0 +1,18 @@
+package com.damai.concert.sysconfig;
+
+/**
+ *
+ * 系统配置参数
+ * Created by Vincent on 2018\4\11 0011.
+ */
+public class SystemCfg {
+
+
+    //普通用户登录使用的realm名称
+    public static final String USER_LOGIN_REALM_NAME = "LoginRealm";
+
+    //后台管理系统登录使用的realm名称
+    public static final String MANAGER_LOGIN_REALM_NAME = "ManagerRealm";
+
+
+}
