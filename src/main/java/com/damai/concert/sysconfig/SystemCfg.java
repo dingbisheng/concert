151f1b4237ad96c729cc737f8de299c5abc5bdbc
zhouxyiii <teorist@outlook.com>
2018-04-13 10:15:53 +0800

    系统配置

--- src/main/java/com/damai/concert/sysconfig/SystemCfg.java
+++ src/main/java/com/damai/concert/sysconfig/SystemCfg.java
@@ -14,5 +14,6 @@ public class SystemCfg {
     //后台管理系统登录使用的realm名称
     public static final String MANAGER_LOGIN_REALM_NAME = "ManagerRealm";
 
-
+    //首页喜好查询5条信息
+    public static  final Integer PAGE_NUM = 5;
 }
