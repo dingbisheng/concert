92ad45e50fb6ef77f95b754b2df93a481413c6c8
zhouxyiii <teorist@outlook.com>
2018-04-14 09:23:20 +0800

    tocken

--- src/main/java/com/damai/concert/realm/token/MyUsernamePasswordToken.java
+++ src/main/java/com/damai/concert/realm/token/MyUsernamePasswordToken.java
@@ -9,12 +9,22 @@ public class MyUsernamePasswordToken extends UsernamePasswordToken {
     //用来标识是自媒体登录，或者是商户登录
     private String switchType;
 
+    private String msgLogin;
+
     public String getSwitchType() {
         return switchType;
     }
 
     public void setSwitchType(String switchType) {
         this.switchType = switchType;
+    }
+
+    public String getMsgLogin() {
+        return msgLogin;
+    }
+
+    public void setMsgLogin(String msgLogin) {
+        this.msgLogin = msgLogin;
     }
 
     public MyUsernamePasswordToken(String username, String password) {
