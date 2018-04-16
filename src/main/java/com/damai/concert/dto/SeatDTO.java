8a362d858099580987e823e688b11c8856353db9
dingbisheng <504503041@qq.com>
2018-04-16 09:34:33 +0800

    4.12   座位DTO

--- src/main/java/com/damai/concert/dto/SeatDTO.java
+++ src/main/java/com/damai/concert/dto/SeatDTO.java
@@ -11,39 +11,11 @@ public class SeatDTO implements Serializable {
     private Integer seatId ;
     private Integer seatCol ;
     private Integer seatRow ;
-
-    @Override
-    public String toString() {
-        return "SeatDTO{" +
-                "seatId=" + seatId +
-                ", seatCol=" + seatCol +
-                ", seatRow=" + seatRow +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof SeatDTO)) return false;
-
-        SeatDTO seatDTO = (SeatDTO) o;
-
-        if (seatId != null ? !seatId.equals(seatDTO.seatId) : seatDTO.seatId != null) return false;
-        if (seatCol != null ? !seatCol.equals(seatDTO.seatCol) : seatDTO.seatCol != null) return false;
-        return seatRow != null ? seatRow.equals(seatDTO.seatRow) : seatDTO.seatRow == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = seatId != null ? seatId.hashCode() : 0;
-        result = 31 * result + (seatCol != null ? seatCol.hashCode() : 0);
-        result = 31 * result + (seatRow != null ? seatRow.hashCode() : 0);
-        return result;
-    }
+    private Integer seatPrice;
+    private Integer seatState;
+    private String seatImg;
 
     public Integer getSeatId() {
-
         return seatId;
     }
 
@@ -65,5 +37,68 @@ public class SeatDTO implements Serializable {
 
     public void setSeatRow(Integer seatRow) {
         this.seatRow = seatRow;
+    }
+
+    public Integer getSeatPrice() {
+        return seatPrice;
+    }
+
+    public void setSeatPrice(Integer seatPrice) {
+        this.seatPrice = seatPrice;
+    }
+
+    public Integer getSeatState() {
+        return seatState;
+    }
+
+    public void setSeatState(Integer seatState) {
+        this.seatState = seatState;
+    }
+
+    public String getSeatImg() {
+        return seatImg;
+    }
+
+    public void setSeatImg(String seatImg) {
+        this.seatImg = seatImg;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SeatDTO seatDTO = (SeatDTO) o;
+
+        if (seatId != null ? !seatId.equals(seatDTO.seatId) : seatDTO.seatId != null) return false;
+        if (seatCol != null ? !seatCol.equals(seatDTO.seatCol) : seatDTO.seatCol != null) return false;
+        if (seatRow != null ? !seatRow.equals(seatDTO.seatRow) : seatDTO.seatRow != null) return false;
+        if (seatPrice != null ? !seatPrice.equals(seatDTO.seatPrice) : seatDTO.seatPrice != null) return false;
+        if (seatState != null ? !seatState.equals(seatDTO.seatState) : seatDTO.seatState != null) return false;
+        return seatImg != null ? seatImg.equals(seatDTO.seatImg) : seatDTO.seatImg == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = seatId != null ? seatId.hashCode() : 0;
+        result = 31 * result + (seatCol != null ? seatCol.hashCode() : 0);
+        result = 31 * result + (seatRow != null ? seatRow.hashCode() : 0);
+        result = 31 * result + (seatPrice != null ? seatPrice.hashCode() : 0);
+        result = 31 * result + (seatState != null ? seatState.hashCode() : 0);
+        result = 31 * result + (seatImg != null ? seatImg.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "SeatDTO{" +
+                "seatId=" + seatId +
+                ", seatCol=" + seatCol +
+                ", seatRow=" + seatRow +
+                ", seatPrice=" + seatPrice +
+                ", seatState=" + seatState +
+                ", seatImg='" + seatImg + '\'' +
+                '}';
     }
 }
